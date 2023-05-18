9bce78c1d6d21984579a5938479dd71d869c87f5
user <user@WIN-GUJTH87OTN8>
2023-05-18 07:44:41 +0300

    Add project files.

---
+++ Metodustv2/AddWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Metodustv2
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        Секретар_ВіділенняEntities db;
+        public AddWindow()
+        {
+            InitializeComponent();
+            db = new Секретар_ВіділенняEntities();
+        }
+
+        private void GroupCB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (GroupCB.Items.Count == 0)
+            {
+                var q =
+                    from Group in db.Група
+                    select new
+                    {
+                        Group.Назва_Групи
+                    };
+                foreach(var it in q)
+                {
+                    GroupCB.Items.Add(it.Назва_Групи);
+                }
+            }
+        }
+
+        private void Student_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (GroupCB.Items.Count != 0 && GroupCB.SelectedItem != null&&Student.Items.Count==0)
+            {
+                var q =
+                    from Stud in db.Студент
+                    join Group in db.Група on Stud.ID_Групи equals Group.ID_Групи
+                    where Group.Назва_Групи == GroupCB.SelectedItem
+                    select new
+                    {
+                        Stud.Прізвище
+                    };
+                foreach(var it in q)
+                {
+                    Student.Items.Add(it.Прізвище);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Відвідування newRec = new Відвідування();
+            var q = 
+                from Student in db.Студент
+                join Group in db.Група on Student.ID_Групи equals Group.ID_Групи
+                where Group.Назва_Групи == GroupCB.SelectedItem && Student.Прізвище==this.Student.SelectedItem
+                select new 
+                {
+                    Student.ID_Студента
+                };
+            int IdOfStudent = 0;
+            foreach(var it in q){
+                IdOfStudent = it.ID_Студента;
+            }
+            try
+            {
+                int max = db.Відвідування.Max(it => it.ID_of_table) + 1;
+                newRec.ID_of_table = max;
+                newRec.ID_Cтудента = IdOfStudent;
+                newRec.Дата_відвідування = DatePick.SelectedDate;
+                newRec.Кількість_пропусків = int.Parse(NumOfPasses.Text);
+                db.Відвідування.Add(newRec);
+                db.SaveChanges();
+                MessageBox.Show("Додавання пройшло успішно");
+            }
+            catch(Exception exc) 
+            {
+                MessageBox.Show("Сталася помилка  - " + exc.Message);
+            }
+        }
+    }
+}
---
+++ Metodustv2/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.Entity;
+using System.Collections;
+
+namespace Metodustv2
+{
+   
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+   
+    public partial class MainWindow : RibbonWindow
+    {
+        
+        abidatEntities db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new abidatEntities();
+
+        }
+
+      
+ 
+      
+      
+
+       
+
+        private void RibbonButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow addWind = new AddWindow();
+            addWind.Show();
+        }
+
+        
+
+      
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ChangeUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            EnterWindow EntWind = new EnterWindow();
+            Close();
+            EntWind.Show();
+        }
+
+        private void StudYearButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
